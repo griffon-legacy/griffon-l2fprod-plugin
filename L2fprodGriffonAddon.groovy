c92ffc739e41f81cf753efda96ba3de3d683c30a
aalmiray <aalmiray@gmail.com>
2012-04-30 21:49:26 +0200

    first commit

---
+++ L2fprodGriffonAddon.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import groovy.swing.factory.RichActionWidgetFactory
+import groovy.swing.factory.TextArgWidgetFactory
+import groovy.swing.factory.TabbedPaneFactory
+import groovy.swing.factory.TableFactory
+import groovy.swing.factory.LayoutFactory
+import griffon.plugins.l2fprod.factory.*
+
+import com.l2fprod.common.swing.*
+import com.l2fprod.common.swing.tips.*
+import com.l2fprod.common.propertysheet.*
+
+/**
+ * @author Andres Almiray
+ */
+class L2fprodGriffonAddon {
+	Map factories = [
+        jbannerPanel: BannerPanel,
+        jbuttonBar: new JButtonBarFactory(),
+        linkButton: new RichActionWidgetFactory(JLinkButton),
+        jtipOfTheDay: JTipOfTheDay,
+        jdefaultTipModel: DefaultTipModel,
+        jdefaultTip: DefaultTip,
+        jtaskPaneGroup: JTaskPaneGroup,
+        jtaskPane: JTaskPane,
+        outlookBar: new TabbedPaneFactory(JOutlookBar),
+        directoryChooser: JDirectoryChooser,
+        fontChooser: JFontChooser,
+        percentLayout: new LayoutFactory(PercentLayout),
+        propertySheetPanel: new PropertySheetPanelFactory(),
+        propertySheetTable: new TableFactory(PropertySheetTable),
+        propertySheetTableModel: new PropertySheetTableModelFactory(),
+        property: new PropertyFactory()
+	]
+}
---
+++ L2fprodGriffonPlugin.groovy
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+class L2fprodGriffonPlugin {
+    // the plugin version
+    String version = '0.1'
+    // the version or versions of Griffon the plugin is designed for
+    String griffonVersion = '0.9.5 > *'
+    // the other plugins this plugin depends on
+    Map dependsOn = [swing: '0.9.5']
+    // resources that are included in plugin packaging
+    List pluginIncludes = []
+    // the plugin license
+    String license = 'Apache Software License 2.0'
+    // Toolkit compatibility. No value means compatible with all
+    // Valid values are: swing, javafx, swt, pivot, qt
+    List toolkits = ['swing']
+    // Platform compatibility. No value means compatible with all
+    // Valid values are:
+    // linux, linux64, windows, windows64, macosx, macosx64, solaris
+    List platforms = []
+    // URL where documentation can be found
+    String documentation = ''
+    // URL where source can be found
+    String source = 'https://github.com/griffon/griffon-l2fprod-plugin'
+
+    List authors = [
+        [
+            name: 'Andres Almiray',
+            email: 'aalmiray@yahoo.com'
+        ]
+    ]
+    String title = 'Swing components from l2fprod'
+    // accepts Markdown syntax. See http://daringfireball.net/projects/markdown/ for details
+    String description = '''
+Missing Swing components provided by [l2fprod][1].
+
+Usage
+-----
+
+The following nodes will become available on a View script upon installing this plugin
+
+| Node                    | Property         | Type     | Required | Bindable | Notes                                                                |
+| ----------------------- | ---------------- | -------- | -------- | -------- | -------------------------------------------------------------------- |
+| jbannerPanel            |                  |          |          |          |                                                                      |
+| jbuttonBar              | ui               | String   | no       | no       | values must be one of [blue, mozilla, icon packager]                 |
+| linkButton              |                  |          |          |          |                                                                      |
+| jtipOfTheDay            |                  |          |          |          |                                                                      |
+| jdefaultTipModel        |                  |          |          |          |                                                                      |
+| jdefaultTip             |                  |          |          |          |                                                                      |
+| jtaskPaneGroup          |                  |          |          |          |                                                                      |
+| jtaskPane               |                  |          |          |          |                                                                      |
+| outlookBar              |                  |          |          |          | same properties as a `tabbedPane`                                    |
+| directoryChooser        |                  |          |          |          |                                                                      |
+| fontChooser             |                  |          |          |          |                                                                      |
+| percentLayout           |                  |          |          |          |                                                                      |
+| propertySheetPanel      | includes         | String[] | no       | no       | filters which properties can be displayed                            |
+|                         | excludes         | String[] | no       | no       | filters which properties can be displayed                            |
+|                         | skipSetup        | boolean  | no       | no       | skips registering all properties, you must specify nested properties |
+|                         |                  |          |          |          | requires a bean as value                                             |
+| propertySheetTable      | includes         | String[] | no       | no       | filters which properties can be displayed                            |
+|                         | excludes         | String[] | no       | no       | filters which properties can be displayed                            |
+| propertySheetTableModel |                  |          |          |          |                                                                      |
+| property                | name             | String   | no       | yes      |                                                                      |
+|                         | displayName      | String   | no       | yes      |                                                                      |
+|                         | shortDescription | String   | no       | yes      |                                                                      |
+|                         | type             | Class    | no       | yes      |                                                                      |
+|                         |                  |          |          |          | requires a bean as value                                             |   
+
+Refer to the Javadocs found in the plugin's distribution to learn more about the properties that can be set on these nodes.
+
+### Example
+
+Using `propertySheetPanel` and `property`
+
+		panel {
+			bean(new Person(), id: "person",
+				name: "Joe",
+				lastname: "Cool")
+			propertySheetPanel(person,
+				excludes: ["metaClass"],
+				preferredSize: [320,300]) {
+					property(name: "name", 	shortDescription: "Person's name")
+					property(name: "lastname", shortDescription: "Person's lastname")  
+			}
+		}
+		
+Using `jtaskPane` and `jtaskPaneGroup`
+
+		jtaskPane(preferredSize: [320,300]) {
+			jtaskPaneGroup(title: "Group 1", special: true) {
+				label("Action 1")
+				label("Action 2")
+			}
+			jtaskPaneGroup(title: "Group 2", expanded: false) {
+				label("Action 1")
+				label("Action 2")
+				label("Action 3")
+			}
+			jtaskPaneGroup(title: "Group 3") {
+				label("Action 1")
+				label("Action 2")
+				label("Action 3")
+			}
+		}
+
+[1]: http://l2fprod.com/common
+'''
+}
